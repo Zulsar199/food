ebc1dbbed7546a904d3079e7afd11117c7fe8038
zuls4r <zulsar.kh@nest.edu.mn>
2025-03-14 10:31:41 +0800

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { userRouter } from "./routes/user.routes.js";
 import { connectToDatabase } from "./database/index.js";
 import { foodRouter } from "./routes/food.routes.js";
@@ -8,11 +9,16 @@ import { foodCategoryRouter } from "./routes/food-category.routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 const port = 8000;
 
 connectToDatabase();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Food Delivery API");
+});
 
 app.use("/user", userRouter);
 app.use("/food", foodRouter);
